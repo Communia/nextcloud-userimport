b484b2e6efd1b94530d5d97dfaf1cfa2c98b8520
Aleix Quintana Alsius <kinta@communia.org>
2026-06-19 13:28:17 +0200

    Support group creations and update user if existing.

--- nextcloud_user_importer.py
+++ nextcloud_user_importer.py
@@ -3,6 +3,7 @@ import os
 import sys
 import csv
 import html
+import json
 import random
 import string
 import requests
@@ -13,6 +14,7 @@ def generate_password():
     return ''.join(random.choices(string.digits, k=10))
 
 def create_user(config, user_data):
+    config.api_url = '/ocs/v1.php/cloud/users'
     url = f"{config.protocol}://{config.admin_name}:{config.admin_pass}@{config.nc_url}{config.api_url}"
     data = {
         'userid': user_data['username'],
@@ -25,35 +27,156 @@ def create_user(config, user_data):
     for group in user_data['groups']:
         data[f'groups[]'] = group
 
+    response = requests.post(url, headers={'OCS-APIRequest': 'true', 'Accept': 'application/json'}, data=data, verify=config.ssl_verify)
+    return response
+
+def update_user(config, user_data):
+    config.api_url = '/ocs/v1.php/cloud/users/' + user_data['username']
+    url = f"{config.protocol}://{config.admin_name}:{config.admin_pass}@{config.nc_url}{config.api_url}"
+    data = {    
+        'displayname': user_data['display_name'],
+        'password': user_data['password'],
+        'email': user_data['email'],
+        'quota': user_data['quota'],
+    }
+    responses = []
+    for key in data:
+        print("Updating:" + key)
+        response =requests.put(url, headers={'OCS-APIRequest': 'true', 'Accept': 'application/json'}, data={'key' : key, 'value' : data[key]}, verify=config.ssl_verify)
+        print(f"User update {user_data['username']}: {response.status_code} - {response.text}")
+    
+    current_groups = user_get_groups(config,user_data['username'])
+    print("  Reassigning groups, first deleting current ones")
+    user_clear_groups(config, user_data['username'],current_groups)
+    print("  Adding new groups")
+    user_add_groups(config, user_data['username'], user_data['groups'])
+
+    return responses
+
+
+# @todo use this when nextcloud 34
+def update_bad_user(config, user_data):
+    config.api_url = '/ocs/v2.php/cloud/users/' + user_data['username'] + '/additional_email'
+    url = f"{config.protocol}://{config.admin_name}:{config.admin_pass}@{config.nc_url}{config.api_url}"
+    data = {
+        'displayName': user_data['display_name'],
+        'password': user_data['password'],
+        'email': user_data['email'],
+        'quota': user_data['quota'],
+        'language': config.language
+    }
+    for group in user_data['groups']:
+        data[f'groups[]'] = group
+
+    print('updating')
+    print(url)
+    print(data)
+    response = requests.put(url, headers={'XDEBUG_SESSION': 'local_ide' ,'OCS-APIRequest': 'true', 'Accept': 'application/json'}, data=data, verify=config.ssl_verify)
+    print(response.content)
+    return response
+
+
+
+def create_group(config, group_data):
+    config.api_url = '/ocs/v1.php/cloud/groups'
+    url = f"{config.protocol}://{config.admin_name}:{config.admin_pass}@{config.nc_url}{config.api_url}"
+    data = {
+        'groupid': group_data['groupid'],
+        'displayName': group_data['display_name']
+    }
+
     response = requests.post(url, headers={'OCS-APIRequest': 'true'}, data=data, verify=config.ssl_verify)
     return response
 
-def main(args):
-    users = []
-    with open(args.csv_file, 'r', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=args.csv_delimiter)
-        for row in reader:
-            user = {
-                'username': html.escape(row['username']),
-                'display_name': html.escape(row['display_name']),
-                'password': generate_password() if args.generate_password else html.escape(row['password']),
-                'email': html.escape(row['email']),
-                'groups': html.escape(row['groups']).split(args.csv_delimiter_groups),
-                'quota': html.escape(row['quota'])
-            }
-            users.append(user)
-
-    print(tabulate([[u['username'], u['display_name'], '*' * len(u['password']), u['email'], u['groups'], u['quota']] for u in users],
-                   headers=['Username', 'Display Name', 'Password', 'Email', 'Groups', 'Quota']))
-
-    if not args.dry_run:
-        input("\nPress Enter to continue with user creation or Ctrl+C to abort...")
-
-        for user in users:
-            response = create_user(args, user)
-            print(f"User {user['username']}: {response.status_code} - {response.text}")
+def user_get_groups(config, username):
+    config.api_url = '/ocs/v1.php/cloud/users/' + username + '/groups'
+    url = f"{config.protocol}://{config.admin_name}:{config.admin_pass}@{config.nc_url}{config.api_url}"
+    response = requests.get(url, headers={'OCS-APIRequest': 'true', 'Accept': 'application/json'}, verify=config.ssl_verify)
+    if response.json()['ocs']['data'] and response.json()['ocs']['data']['groups']:
+        return response.json()['ocs']['data']['groups']
     else:
-        print("\nDry run completed. No users were created.")
+        return []
+
+def user_clear_groups(config, username, groups):
+    config.api_url = '/ocs/v1.php/cloud/users/' + username + '/groups'
+    url = f"{config.protocol}://{config.admin_name}:{config.admin_pass}@{config.nc_url}{config.api_url}"
+    responses = []
+    for group in groups:
+        response = requests.delete(url, headers={'OCS-APIRequest': 'true', 'Accept': 'application/json'}, data={'groupid': group }, verify=config.ssl_verify)
+        print(f"  User clear group {group} {username}: {response.status_code} - {response.text}")
+        responses.append(response)
+    return responses
+
+def user_add_groups(config, username, groups):
+    config.api_url = '/ocs/v1.php/cloud/users/' + username + '/groups'
+    url = f"{config.protocol}://{config.admin_name}:{config.admin_pass}@{config.nc_url}{config.api_url}"
+    responses = []
+    for group in groups:
+        print(f"  Add {username} to {group}")
+        response = requests.post(url, headers={'OCS-APIRequest': 'true', 'Accept': 'application/json'}, data={'groupid': group }, verify=config.ssl_verify)
+        print(f"  User add group {group} {username}: {response.status_code} - {response.text}")
+        responses.append(response)
+
+
+    return responses
+
+
+def main(args):
+    if args.users_csv_file:
+        users = []
+        with open(args.users_csv_file, 'r', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=args.csv_delimiter)
+            for row in reader:
+                user = {
+                    'username': html.escape(row['username']),
+                    'display_name': html.escape(row['display_name']),
+                    'password': generate_password() if args.generate_password else html.escape(row['password']),
+                    'email': html.escape(row['email']),
+                    'groups': html.escape(row['groups']).split(args.csv_delimiter_groups),
+                    'quota': html.escape(row['quota'])
+                }
+                users.append(user)
+
+        print(tabulate([[u['username'], u['display_name'], '*' * len(u['password']), u['email'], u['groups'], u['quota']] for u in users],
+                       headers=['Username', 'Display Name', 'Password', 'Email', 'Groups', 'Quota']))
+
+        if not args.dry_run:
+            input("\nPress Enter to continue with user creation or Ctrl+C to abort...")
+
+            for user in users:
+                response = create_user(args, user)
+                print(response.json()['ocs']['meta']['statuscode'])
+                print(f"User {user['username']}: {response.status_code} - {response.text}")
+                #Already exists:
+                if response.json()['ocs']['meta']['statuscode'] == 102:
+                    print("Already there, updating")
+                    update_user(args,user)
+
+        else:
+            print("\nDry run completed. No users were created.")
+    if args.groups_csv_file:
+        groups = []
+        with open(args.groups_csv_file, 'r', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=args.csv_delimiter)
+            for row in reader:
+                group = {
+                    'groupid': html.escape(row['username']),
+                    'display_name': html.escape(row['display_name'])
+                }
+                groups.append(group)
+
+        print(tabulate([[u['groupid'], u['display_name']] for g in groups],
+                       headers=['Group id', 'Display Name']))
+
+        if not args.dry_run:
+            input("\nPress Enter to continue with group creation or Ctrl+C to abort...")
+
+            for group in groups:
+                response = create_group(args, group)
+                print(f"Group {group['groupid']}: {response.status_code} - {response.text}")
+        else:
+            print("\nDry run completed. No users were created.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Nextcloud User Importer")
@@ -62,7 +185,9 @@ if __name__ == "__main__":
     parser.add_argument("--admin-name", required=True, help="Admin username")
     parser.add_argument("--admin-pass", required=True, help="Admin password")
     parser.add_argument("--api-url", default="/ocs/v1.php/cloud/users", help="API URL")
-    parser.add_argument("--csv-file", required=True, help="Path to CSV file")
+    options = parser.add_mutually_exclusive_group(required=True)
+    options.add_argument("--users-csv-file", help="Path to CSV file")
+    options.add_argument("--groups-csv-file", help="Path to CSV file")
     parser.add_argument("--csv-delimiter", default=",", help="CSV delimiter")
     parser.add_argument("--csv-delimiter-groups", default=";", help="CSV delimiter for groups")
     parser.add_argument("--generate-password", action="store_true", help="Generate random passwords")
