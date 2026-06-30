1febfd3505a43bab9c70678870b7cbc66e368427
Aleix Quintana Alsius <kinta@communia.org>
2026-06-30 16:28:33 +0200

    Split actions in commands. Add mail-app command.

--- README.md
+++ README.md
@@ -1,38 +1,43 @@
-# Import Nextcloud users from a CSV file
+# Perform nextcloud provision tasks from a CSV file
 
 Credit to [t-markmann](https://github.com/t-markmann/nc-userimporter) for inspiration.
+
+It helps you provisioning users, groups from csv, or helping in some other provisioning tasks
+such as dumping the current users, or showing the nextcloud mail app command to create mail 
+accounts for mail client app.
 
 Quick Usage:
 
 ```
-python3 nextcloud_user_importer.py --nc-url your-nextcloud-url.com --admin-name admin --admin-pass password --csv-file users.csv
+python3 nextcloud_user_importer.py users -nc-url your-nextcloud-url.com --admin-name admin --admin-pass password --csv-file users.csv
 ```
 
 For help:
 
 ```
-python3 nextcloud_user_importer.py --help
+python3 nextcloud_user_importer.py {command} --help
 ```
 
-```
-usage: nextcloud_user_importer.py [-h] [--protocol PROTOCOL] --nc-url NC_URL --admin-name ADMIN_NAME --admin-pass ADMIN_PASS [--api-url API_URL] (--users-csv-file USERS_CSV_FILE | --groups-csv-file GROUPS_CSV_FILE | --dump-users-csv) [--csv-delimiter CSV_DELIMITER] [--csv-delimiter-groups CSV_DELIMITER_GROUPS] [--generate-password] [--no-ssl-verify] [--language LANGUAGE] [--dry-run] [--password]
+Where commands are users, groups, dump, and mail-app.
 
-Nextcloud User Importer
+# Users
+
+```
+usage: nextcloud_user_importer.py users [-h] [--users-csv-file USERS_CSV_FILE] [--protocol {http,https}] --nc-url NC_URL --admin-name ADMIN_NAME --admin-pass ADMIN_PASS [--api-url API_URL] [--csv-delimiter CSV_DELIMITER] [--csv-delimiter-groups CSV_DELIMITER_GROUPS]
+                                        [--generate-password] [--no-ssl-verify] [--language LANGUAGE] [--dry-run] [--password]
 
 options:
   -h, --help            show this help message and exit
-  --protocol PROTOCOL   Protocol (http or https)
+  --users-csv-file USERS_CSV_FILE
+                        Path to CSV file
+  --protocol {http,https}
+                        Protocol (http or https)
   --nc-url NC_URL       Nextcloud URL
   --admin-name ADMIN_NAME
                         Admin username
   --admin-pass ADMIN_PASS
                         Admin password
   --api-url API_URL     API URL
-  --users-csv-file USERS_CSV_FILE
-                        Path to CSV file
-  --groups-csv-file GROUPS_CSV_FILE
-                        Path to CSV file
-  --dump-users-csv      Dump current users
   --csv-delimiter CSV_DELIMITER
                         CSV delimiter
   --csv-delimiter-groups CSV_DELIMITER_GROUPS
@@ -42,12 +47,13 @@ options:
   --language LANGUAGE   User language
   --dry-run             Perform a dry run without creating users
   --password            Update password processing when updating user
+
 ```
 
 
 ## Importing Users
 
-If adding users your CSV users file should look like this:
+If you want to add users, your CSV users file should look like this:
 
 ```
 username,display_name,password,email,groups,quota
@@ -64,20 +70,45 @@ If you want to update also the password add the argument `--password`.
 
 
 You can obtain the current users list with the argument `--dump-users-csv`,
-this may be useful as a template.
+this may be useful as a template. (more information on dump section below).
 
-`python3 nextcloud_user_importer.py --nc-url your-nextcloud-url.com --admin-name admin --admin-pass password --dump-users-csv`
-
-**Note about passwords** : Take care that plain password cannot be 
-obtained so if importing new users using this dump, password field must be 
-changed or use the `--generate-password` to generate a new one automatically.
-Otherwise passwords will be set to `*CHANGEME*` value.
+`python3 nextcloud_user_importer.py dump --nc-url your-nextcloud-url.com --admin-name admin --admin-pass password --dump-users-csv`
 
 
 
-## Groups
+**Note about passwords** : Take care that plain password cannot be
+obtained so if importing new users using this dump, password field in csv must
+be changed or use the `--generate-password` to generate a new one
+automatically. Otherwise passwords will be set to `*CHANGEME*` value.
 
-If adding groups your CSV groups file should look like this:
+
+# Groups
+
+```
+usage: nextcloud_user_importer.py groups [-h] [--groups-csv-file GROUPS_CSV_FILE] [--protocol {http,https}] --nc-url NC_URL --admin-name ADMIN_NAME --admin-pass ADMIN_PASS [--api-url API_URL] [--csv-delimiter CSV_DELIMITER] [--no-ssl-verify] [--dry-run]
+
+options:
+  -h, --help            show this help message and exit
+  --groups-csv-file GROUPS_CSV_FILE
+                        Path to CSV file
+  --protocol {http,https}
+                        Protocol (http or https)
+  --nc-url NC_URL       Nextcloud URL
+  --admin-name ADMIN_NAME
+                        Admin username
+  --admin-pass ADMIN_PASS
+                        Admin password
+  --api-url API_URL     API URL
+  --csv-delimiter CSV_DELIMITER
+                        CSV delimiter
+  --no-ssl-verify       Disable SSL verification
+  --dry-run             Perform a dry run without creating groups
+```
+
+
+## Importing groups
+
+If you wnat to add groups your CSV groups file should look like this:
 
 ```
 groupid,display_name
@@ -88,5 +119,77 @@ engineering,Engineering
 
 And groups can be imported running:
 
-`python3 nextcloud_user_importer.py --nc-url your-nextcloud-url.com --admin-name admin --admin-pass password --groups-csv-file groups.csv`
+`python3 nextcloud_user_importer.py groups --nc-url your-nextcloud-url.com --admin-name admin --admin-pass password --groups-csv-file groups.csv`
+
+
+# Dump
+
+It is useful to obtain a list of current users, this csv could be used also as
+a template to be used for importing users with the `users` command (check above
+for notes).
+
+```
+usage: nextcloud_user_importer.py dump [-h] [--protocol {http,https}] --nc-url NC_URL --admin-name ADMIN_NAME --admin-pass ADMIN_PASS [--api-url API_URL] [--csv-delimiter CSV_DELIMITER] [--no-ssl-verify] [--dry-run]
+
+options:
+  -h, --help            show this help message and exit
+  --protocol {http,https}
+                        Protocol (http or https)
+  --nc-url NC_URL       Nextcloud URL
+  --admin-name ADMIN_NAME
+                        Admin username
+  --admin-pass ADMIN_PASS
+                        Admin password
+  --api-url API_URL     API URL
+  --csv-delimiter CSV_DELIMITER
+                        CSV delimiter
+  --no-ssl-verify       Disable SSL verification
+  --dry-run             Perform a dry run without creating dump
+```
+
+
+# Mail app
+
+This action will not perform any action, isntead will show the command to be run
+on the server, right now there's no api endpoint to create mail app accounts.
+
+```
+usage: nextcloud_user_importer.py mail-app [-h] --mail-csv-file MAIL_CSV_FILE [--generate-password] [--csv-delimiter CSV_DELIMITER] --imap-host IMAP_HOST --imap-port IMAP_PORT --imap-ssl-mode {ssl,tls,none} --smtp-host SMTP_HOST --smtp-port SMTP_PORT --smtp-ssl-mode
+                                           {ssl,tls,none} [--auth-method {password,xoauth2}]
+
+options:
+  -h, --help            show this help message and exit
+  --mail-csv-file MAIL_CSV_FILE
+                        Path to CSV file as in users csv to out occ script to be run in server
+  --generate-password   Generate random passwords
+  --csv-delimiter CSV_DELIMITER
+                        CSV delimiter
+  --imap-host IMAP_HOST
+  --imap-port IMAP_PORT
+  --imap-ssl-mode {ssl,tls,none}
+  --smtp-host SMTP_HOST
+  --smtp-port SMTP_PORT
+  --smtp-ssl-mode {ssl,tls,none}
+  --auth-method {password,xoauth2}
+
+```
+
+## Creating mail accounts
+
+Based on the same csv structure as users command, an example could be this users.csv:
+
+```
+username,display_name,password,email,groups,quota
+bob,Robert Wang,,bob@example.com,devs,5 GB
+```
+
+run suited to your needs
+
+```
+$ python nextcloud_user_importer.py mail-app --mail-csv-file users.csv --imap-host mails.example.org --imap-port 993  --imap-ssl-mode ssl --smtp-host mails.example.org --smtp-port 465 --smtp-ssl-mode ssl 
+occ mail:account:create bob bob bob@example.com mails.example.org 993 ssl bob@example.com  mails.example.org 465 ssl bob@example.com  password
+```
+
+Copy the output (`occ mail:account:create ...` ). Login to your server, go to nextcloud directory. and paste the
+the commands.
 
--- nextcloud_user_importer.py
+++ nextcloud_user_importer.py
@@ -80,7 +80,7 @@ def update_bad_user(config, user_data):
 
 
 def create_group(config, group_data):
-    config.api_url = '/ocs/v1.php/cloud/groups'
+    #config.api_url = '/ocs/v1.php/cloud/groups'
     url = f"{config.protocol}://{config.admin_name}:{config.admin_pass}@{config.nc_url}{config.api_url}"
     data = {
         'groupid': group_data['groupid'],
@@ -134,7 +134,7 @@ def get_users(config):
 
 
 def main(args):
-    if args.users_csv_file:
+    if hasattr(args, 'users_csv_file'):
         users = []
         with open(args.users_csv_file, 'r', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile, delimiter=args.csv_delimiter)
@@ -166,7 +166,7 @@ def main(args):
 
         else:
             print("\nDry run completed. No users were created.")
-    if args.groups_csv_file:
+    if hasattr(args, 'groups_csv_file'):
         groups = []
         with open(args.groups_csv_file, 'r', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile, delimiter=args.csv_delimiter)
@@ -188,27 +188,92 @@ def main(args):
                 print(f"Group {group['groupid']}: {response.status_code} - {response.text}")
         else:
             print("\nDry run completed. No users were created.")
-    if args.dump_users_csv:
+    if hasattr(args, 'dump_users_csv'):
         get_users(args)
+    if hasattr(args, 'mail_csv_file'):
+        users = []
+        with open(args.mail_csv_file, 'r', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=args.csv_delimiter)
+            for row in reader:
+                user = {
+                    'username': html.escape(row['username']),
+                    'display_name': html.escape(row['display_name']),
+                    'password': generate_password() if args.generate_password else html.escape(row['password']),
+                    'email': html.escape(row['email'])
+                }
+                print('occ mail:account:create {user_id} {name} {email} {imap_host} {imap_port} {imap_ssl_mode} {imap_user} {imap_password} {smtp_host} {smtp_port} {smtp_ssl_mode} {smtp_user} {smtp_password} {auth_method}'.format(
+                    user_id = user['username'],
+                    name = user['username'],
+                    email = user['email'],
+                    imap_host = args.imap_host,
+                    imap_port = args.imap_port,
+                    imap_ssl_mode = args.imap_ssl_mode,
+                    imap_user = user['email'],
+                    imap_password = user['password'],
+                    smtp_host = args.smtp_host,
+                    smtp_port = args.smtp_port,
+                    smtp_ssl_mode = args.smtp_ssl_mode,
+                    smtp_user = user['email'],
+                    smtp_password = user['password'],
+                    auth_method = args.auth_method
+                ))
+
+ 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Nextcloud User Importer")
-    parser.add_argument("--protocol", default="https", help="Protocol (http or https)")
-    parser.add_argument("--nc-url", required=True, help="Nextcloud URL")
-    parser.add_argument("--admin-name", required=True, help="Admin username")
-    parser.add_argument("--admin-pass", required=True, help="Admin password")
-    parser.add_argument("--api-url", default="/ocs/v1.php/cloud/users", help="API URL")
-    options = parser.add_mutually_exclusive_group(required=True)
-    options.add_argument("--users-csv-file", help="Path to CSV file")
-    options.add_argument("--groups-csv-file", help="Path to CSV file")
-    options.add_argument("--dump-users-csv", action="store_true", help="Dump current users")
-    parser.add_argument("--csv-delimiter", default=",", help="CSV delimiter")
-    parser.add_argument("--csv-delimiter-groups", default=";", help="CSV delimiter for groups")
-    parser.add_argument("--generate-password", action="store_true", help="Generate random passwords")
-    parser.add_argument("--no-ssl-verify", action="store_false", dest="ssl_verify", help="Disable SSL verification")
-    parser.add_argument("--language", default="en", help="User language")
-    parser.add_argument("--dry-run", action="store_true", help="Perform a dry run without creating users")
-    parser.add_argument("--password", action="store_true", help="Update password processing when updating user")
+    subparsers = parser.add_subparsers(title='subcommands', description='valid subcommands', help='additional subcommands')
+    mail_app_parser = subparsers.add_parser('mail-app')
+    mail_app_parser.add_argument('--mail-csv-file', required=True, help="Path to CSV file as in users csv to out occ script to be run in server")
+    mail_app_parser.add_argument("--generate-password", action="store_true", help="Generate random passwords")
+    mail_app_parser.add_argument("--csv-delimiter", default=",", help="CSV delimiter")
+    mail_app_parser.add_argument('--imap-host', required=True)
+    mail_app_parser.add_argument('--imap-port', required=True)
+    mail_app_parser.add_argument('--imap-ssl-mode', required=True, choices=["ssl", "tls", "none"])
+    mail_app_parser.add_argument('--smtp-host', required=True)
+    mail_app_parser.add_argument('--smtp-port', required=True)
+    mail_app_parser.add_argument('--smtp-ssl-mode', required=True, choices=["ssl", "tls", "none"])
+    mail_app_parser.add_argument('--auth-method', default="password", choices=["password", "xoauth2"])
+
+    users_parser = subparsers.add_parser('users')
+    users_parser.add_argument("--users-csv-file", help="Path to CSV file")
+    users_parser.add_argument("--protocol", default="https", help="Protocol (http or https)", choices=["http", "https"])
+    users_parser.add_argument("--nc-url", required=True, help="Nextcloud URL")
+    users_parser.add_argument("--admin-name", required=True, help="Admin username")
+    users_parser.add_argument("--admin-pass", required=True, help="Admin password")
+    users_parser.add_argument("--api-url", default="/ocs/v1.php/cloud/users", help="API URL")
+    users_parser.add_argument("--csv-delimiter", default=",", help="CSV delimiter")
+    users_parser.add_argument("--csv-delimiter-groups", default=";", help="CSV delimiter for groups")
+    users_parser.add_argument("--generate-password", action="store_true", help="Generate random passwords")
+    users_parser.add_argument("--no-ssl-verify", action="store_false", dest="ssl_verify", help="Disable SSL verification")
+    users_parser.add_argument("--language", default="en", help="User language")
+    users_parser.add_argument("--dry-run", action="store_true", help="Perform a dry run without creating users")
+    users_parser.add_argument("--password", action="store_true", help="Update password processing when updating user")
+
+    groups_parser = subparsers.add_parser('groups')
+    groups_parser.add_argument("--groups-csv-file", help="Path to CSV file")
+    groups_parser.add_argument("--protocol", default="https", help="Protocol (http or https)", choices=["http", "https"])
+    groups_parser.add_argument("--nc-url", required=True, help="Nextcloud URL")
+    groups_parser.add_argument("--admin-name", required=True, help="Admin username")
+    groups_parser.add_argument("--admin-pass", required=True, help="Admin password")
+    groups_parser.add_argument("--api-url", default="/ocs/v1.php/cloud/groups", help="API URL")
+    groups_parser.add_argument("--csv-delimiter", default=",", help="CSV delimiter")
+    groups_parser.add_argument("--no-ssl-verify", action="store_false", dest="ssl_verify", help="Disable SSL verification")
+    groups_parser.add_argument("--dry-run", action="store_true", help="Perform a dry run without creating groups")
+
+
+    dump_parser = subparsers.add_parser('dump')
+    dump_parser.add_argument("--protocol", default="https", help="Protocol (http or https)", choices=["http", "https"])
+    dump_parser.add_argument("--nc-url", required=True, help="Nextcloud URL")
+    dump_parser.add_argument("--admin-name", required=True, help="Admin username")
+    dump_parser.add_argument("--admin-pass", required=True, help="Admin password")
+    dump_parser.add_argument("--api-url", default="/ocs/v1.php/cloud/users/details", help="API URL")
+    dump_parser.add_argument("--csv-delimiter", default=",", help="CSV delimiter")
+    dump_parser.add_argument("--no-ssl-verify", action="store_false", dest="ssl_verify", help="Disable SSL verification")
+    dump_parser.add_argument("--dry-run", action="store_true", help="Perform a dry run without creating dump")
+
+
+
 
     args = parser.parse_args()
     
